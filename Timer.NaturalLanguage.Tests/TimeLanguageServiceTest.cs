ea925a584284ef135edc1d9b6dcbfeb7094e07c3
Paul Stempel <Paul.stempel@gmx.de>
2017-10-07 15:08:07 +0200

    test fix

--- Timer.NaturalLanguage.Tests/TimeLanguageServiceTest.cs
+++ Timer.NaturalLanguage.Tests/TimeLanguageServiceTest.cs
@@ -14,7 +14,7 @@ namespace NaturalLanguage.Tests
             var result = service.Analyse("Erstelle eine Zeiterfassung von 12:23 bis 14:13 auf das Ticket INC134");
 
             Assert.IsNotNull(result);
-            Assert.AreEqual("INC123", result.TicketId.ToUpper()); 
+            Assert.AreEqual("INC134", result.TicketId.ToUpper()); 
             Assert.AreEqual(DateTime.Parse("12:23"), result.TimeStamp);
         }
     }
