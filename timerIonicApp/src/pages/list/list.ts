96ef90fb66209a4e0ed5cb704547691eb78d4154
sven_s <sven@sven-s.de>
2017-10-07 15:52:34 +0200

    display list entries in ionic

--- timerIonicApp/src/pages/list/list.ts
+++ timerIonicApp/src/pages/list/list.ts
@@ -1,31 +1,45 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'page-list',
   templateUrl: 'list.html'
 })
 export class ListPage {
-  selectedItem: any;
-  icons: string[];
-  items: Array<{title: string, note: string, icon: string}>;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    // If we navigated to this page, we will have an item available as a nav param
-    this.selectedItem = navParams.get('item');
+  tasks: any;
 
-    // Let's populate this page with some filler content for funzies
-    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    'american-football', 'boat', 'bluetooth', 'build'];
 
-    this.items = [];
-    for (let i = 1; i < 11; i++) {
-      this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-      });
-    }
+
+  // selectedItem: any;
+  // icons: string[];
+  // items: Array<{title: string, note: string, icon: string}>;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http) {
+
+    this.http.get('http://localhost:50682/api/tasks').map(res => res.json()).subscribe(data => {
+      console.log(data);
+      this.tasks = data;
+    });
+
+
+    // // If we navigated to this page, we will have an item available as a nav param
+    // this.selectedItem = navParams.get('item');
+
+    // // Let's populate this page with some filler content for funzies
+    // this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
+    // 'american-football', 'boat', 'bluetooth', 'build'];
+
+    // this.items = [];
+    // for (let i = 1; i < 11; i++) {
+    //   this.items.push({
+    //     title: 'Item ' + i,
+    //     note: 'This is item #' + i,
+    //     icon: this.icons[Math.floor(Math.random() * this.icons.length)]
+    //   });
+    // }
   }
 
   itemTapped(event, item) {
