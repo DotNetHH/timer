f1f09c5e7047ce8a931d4b63a11282b37ea3e07e
framag <fmagnus@gmx.de>
2017-10-07 15:37:28 +0200

    returning badrequest and ok for post methods

--- Timer.WebApi/Controllers/TasksController.cs
+++ Timer.WebApi/Controllers/TasksController.cs
@@ -1,11 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using WebApi.Models;
-
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace WebApi.Controllers
 {
@@ -31,52 +27,58 @@ namespace WebApi.Controllers
             return "value";
         }
 
-        // POST api/tasks
+        // POST api/tasks/start
         /// <summary>
         /// This method starts a task.
         /// This method accepts JSON object as parameter that contains UTC TimeStamp at least. Further properties may follow.
         /// </summary>
         /// <param name="jsonTask">Task object as JSON, containing UTC Timestamp at least</param>
         [HttpPost]
-        public void Start([FromBody] TaskModel task)
+        public ActionResult Start([FromBody] TaskModel task)
         {
             if (task == null)
-                throw new ArgumentNullException();
+                return BadRequest();
 
             var startCommand = AutoMapper.Mapper.Map<TaskModel, Timer.Abstractions.StartTaskCommand>(task);
             businessLayerCommandManager.AddCommand(startCommand);
+
+            return Ok();
         }
 
-        // POST api/tasks
+        // POST api/tasks/stop
         /// <summary>
         /// This method stops a task.
         /// This method accepts JSON object as parameter that contains UTC TimeStamp at least. Further properties may follow.
         /// </summary>
         /// <param name="jsonTask">Task object as JSON, containing UTC Timestamp at least</param>
         [HttpPost]
-        public void Stop([FromBody] TaskModel task)
+        public ActionResult Stop([FromBody] TaskModel task)
         {
             if (task == null)
-                throw new ArgumentNullException();
+                return BadRequest();
 
             var stopCommand = AutoMapper.Mapper.Map<TaskModel, Timer.Abstractions.StopTaskCommand>(task);
             businessLayerCommandManager.AddCommand(stopCommand);
+
+            return Ok();
         }
 
-        // POST api/tasks
+        // POST api/tasks/interrupt
         /// <summary>
         /// This method interrupts a task. When the interrupt is terminated, the original task continues.
         /// This method accepts JSON object as parameter that contains UTC TimeStamp at least. Further properties may follow.
         /// </summary>
         /// <param name="jsonTask">Task object as JSON, containing UTC Timestamp at least</param>
         [HttpPost]
-        public void Interrupt([FromBody] TaskModel task)
+        public ActionResult Interrupt([FromBody] TaskModel task)
         {
             if (task == null)
-                throw new ArgumentNullException();
+                return BadRequest();
 
             var interruptCommand = AutoMapper.Mapper.Map<TaskModel, Timer.Abstractions.InterruptCommand>(task);
             businessLayerCommandManager.AddCommand(interruptCommand);
+
+            return Ok();
         }
     }
 }
