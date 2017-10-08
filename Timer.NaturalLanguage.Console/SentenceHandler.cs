88a8e658f050948dd47928118be8e1e516e4f1ee
Paul Stempel <Paul.stempel@gmx.de>
2017-10-08 10:31:20 +0200

    console refactoring

---
+++ Timer.NaturalLanguage.Console/SentenceHandler.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using MJNsoft.Base.DependencyInjection;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Timer.Abstractions;
+
+namespace Timer.NaturalLanguage.Console
+{
+    public class SentenceHandler : ISentenceHandler
+    {
+        private readonly TimerNaturalLanguageService _naturalLanguageService;
+        private readonly ICommandManager _commandManager;
+
+        public SentenceHandler(TimerNaturalLanguageService naturalLanguageService, ICommandManager commandManager)
+        {
+            _naturalLanguageService = naturalLanguageService;
+            _commandManager = commandManager;
+        }
+
+        public void Handle(string sentence)
+        {
+            var analysedsentence = _naturalLanguageService.Analyse(sentence);
+
+            System.Console.WriteLine("Erkannt wurde: ");
+            System.Console.WriteLine(JsonConvert.SerializeObject(analysedsentence));
+
+            switch (analysedsentence.Intent)
+            {
+                case AnalysedSentenceIntent.StartTimeRecordingNow:
+                    Start(analysedsentence);
+                    break;
+                case AnalysedSentenceIntent.StopTimeRecordingNow:
+                    Stop(analysedsentence);
+                    break;
+                case AnalysedSentenceIntent.ShowTimeRecording:
+                    Show();
+                    break;
+            }
+        }
+
+        private void Start(AnalysedSentence analysedsentence)
+        {
+            var command = new StartTaskCommand()
+            {
+                TimeStamp = DateTime.Now,
+                Description = analysedsentence.GetEntityOrEmpty(AnalysedSentenceEntity.Description),
+                TicketId = analysedsentence.GetEntityOrEmpty(AnalysedSentenceEntity.Incident)
+            };
+            _commandManager.AddCommand(command);
+        }
+
+        private void Stop(AnalysedSentence analysedsentence)
+        {
+            var  command = new StopTaskCommand()
+            {
+                TimeStamp = DateTime.Now,
+                Description = analysedsentence.GetEntityOrEmpty(AnalysedSentenceEntity.Description),
+                TicketId = analysedsentence.GetEntityOrEmpty(AnalysedSentenceEntity.Incident)
+            };
+            _commandManager.AddCommand(command);
+        }
+
+
+        private void Show()
+        {
+            foreach (var command in this._commandManager.GetAll())
+            {
+                System.Console.WriteLine(command.GetType().Name + " " + JsonConvert.SerializeObject(command));
+            }
+        }
+    }
+}
---
+++ Timer.NaturalLanguage/AnalysedSentence.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Timer.NaturalLanguage
+{
+    public class AnalysedSentence
+    {
+        public AnalysedSentenceIntent Intent { get; set; }
+        public Dictionary<AnalysedSentenceEntity, string> Entities { get; set; }
+        public double Score { get; set; }
+
+        public string GetEntityOrEmpty(AnalysedSentenceEntity entity)
+        {
+            if (Entities != null && Entities.ContainsKey(entity))
+            {
+                return Entities[entity];
+            }
+
+            return string.Empty;
+        }
+    }
+}
