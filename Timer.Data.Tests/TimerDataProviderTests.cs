19565e30e0f6548aa66181fb4763df4421bd7a5f
Michael J. Neffe <michael.neffe@MJNsoft.de>
2017-10-08 12:58:05 +0200

    TimerDataProviderTests

--- Timer.Data.Tests/TimerDataProviderTests.cs
+++ Timer.Data.Tests/TimerDataProviderTests.cs
@@ -1,16 +1,34 @@
-﻿using MJNsoft.Base.DependencyInjection;
-using NUnit.Framework;
-using Timer.Data.Abstractions;
+﻿//using MJNsoft.Base.Database.Abstractions;
+//using MJNsoft.Base.DependencyInjection;
+//using NUnit.Framework;
+//using Timer.Data.Abstractions;
+//using System.Linq;
 
-namespace Timer.Data.Tests
-{
-    [TestFixture]
-    public class TimerDataProviderTests
-    {
-        [Test]
-        public void CommandRepository_ReturnsRepository()
-        {
-            Assert.That(IoC.Services.GetService<ITimerDataProvider>().CommandRepository, Is.Not.Null);
-        }
-    }
-}
+//namespace Timer.Data.Tests
+//{
+//    [TestFixture]
+//    public class TimerDataProviderTests
+//    {
+//        private ITimerDataProvider _timerDataProvider;
+//        private IRepositoryWorker<CommandEntity, TimerDbContext> _timerRepositoryWorker;
+
+//        public TimerDataProviderTests()
+//        {
+//            _timerDataProvider = IoC.Services.GetService<ITimerDataProvider>();
+//        }
+
+//        [Test]
+//        public void CommandRepository_ReturnsRepository()
+//        {
+//            _timerDataProvider.AddEvent(new TimerEvent { EventType = TimerEventType.Start, TimeStamp = new System.DateTime(2017, 10, 8, 9, 0, 0) });
+//            _timerDataProvider.AddEvent(new TimerEvent { EventType = TimerEventType.Stop, TimeStamp = new System.DateTime(2017, 10, 8, 11, 0, 0) });
+
+//            Assert.That(_timerDataProvider.GetAllForDate(new System.DateTime(2017,10,8)).Count(), Is.EqualTo(2));
+
+//            // Aufräumen
+//            var inserted = _timerRepositoryWorker.GetAll();
+//            foreach(var @event in inserted)
+//                _timerRepositoryWorker.Delete(@event.Id);
+//        }
+//    }
+//}
